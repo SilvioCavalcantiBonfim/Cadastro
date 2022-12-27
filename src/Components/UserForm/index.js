3770c1312c1095c09b67ba21af9af9999a986178
SILVIO CAVALCANTI BONFIM <silviocavalcanti2011@gmail.com>
2022-12-27 02:20:01 -0300

    update

--- src/Components/UserForm/index.js
+++ src/Components/UserForm/index.js
@@ -3,22 +3,33 @@ import { UserEntity } from '../../Entities/userEntity';
 import { AiOutlineUserAdd } from 'react-icons/ai';
 import './index.css';
 
+const validation = (state, action) => {
+    if(action.type === 'UPDATE_NAME')
+        return {...state,name: {value: action.val, isValid: true}};
+    if(action.type === 'FOCUSOUT_NAME')
+        return {...state,name: {value: state.name.value, isValid: state.name.value !== ''}};
+    if(action.type === 'UPDATE_AGE')
+        return {...state,age: {value: action.val, isValid: true}};
+    if(action.type === 'FOCUSOUT_AGE')
+        return {...state,age: {value: state.age.value, isValid: state.age.value > 0}};
+    if(action.type === 'RESET')
+        return {name: {value: '', isValid: true}, age: {value: 0, isValid: true}};
+}
+
 const UserForm = (props) => {
-    const [Value, setValue] = React.useState({ name: '', age: 0 });
-    const [isValid, setValid] = React.useState([true, true]);
+    const [Value, dispatcherValue] = React.useReducer(validation, {name: {value: '', isValid: true}, age: {value: 0, isValid: true}});
 
     return (
         <div className='container'>
             <form onSubmit={(e) => {
                 e.preventDefault();
-                var _isValid = [(Value.name !== ''), (Value.age > 0)]
-                if (_isValid.indexOf(false) === -1) {
-                    props.setUser(v => v.concat([new UserEntity(null, Value.name, Value.age)]));
-                    setValue({ name: '', age: 0 });
-                    setValid([true, true]);
+                if (Value.name.value !== '' && Value.age.value > 0) {
+                    props.setUser(v => v.concat([new UserEntity(null, Value.name.value, Value.age.value)]));
+                    dispatcherValue({type: 'RESET'});
+                }else{
+                    dispatcherValue({type: 'FOCUSOUT_NAME'})
+                    dispatcherValue({type: 'FOCUSOUT_AGE'})
                 }
-                else
-                    setValid(_isValid);
             }}>
                 <div className='title'>
                     <AiOutlineUserAdd />
@@ -28,15 +39,16 @@ const UserForm = (props) => {
                     <div className='formControlLabel'>Nome</div>
                     <div className='formControlInputContainer'>
                         <input
-                            className={`formControlInput${(!isValid[0]) ? ' error' : ''}`}
+                            className={`formControlInput${(!Value.name.isValid) ? ' error' : ''}`}
                             name='name'
-                            value={Value.name}
+                            value={Value.name.value}
                             autoComplete='off'
                             placeholder='Ex.: Regina'
-                            onChange={(e) => setValue(v => { return { ...v, name: e.target.value } })}
+                            onBlur={() => dispatcherValue({type: 'FOCUSOUT_NAME'})}
+                            onChange={(e) => dispatcherValue({type: 'UPDATE_NAME', val: e.target.value})}
                         />
                     </div>
-                    <div className='errorMsg'> {!isValid[0] && `Por favor, indique um nome válido.`}</div>
+                    <div className='errorMsg'> {!Value.name.isValid && `Por favor, indique um nome válido.`}</div>
                 </div>
                 <div className='formControl'>
                     <div className='formControlLabel'>Idade</div>
@@ -46,13 +58,14 @@ const UserForm = (props) => {
                             min={0}
                             max={120}
                             name='age'
-                            value={Value.age}
-                            className={`formControlInput${(!isValid[1]) ? ' error' : ''}`}
+                            value={Value.age.value}
+                            className={`formControlInput${(!Value.age.isValid) ? ' error' : ''}`}
                             placeholder='0'
-                            onChange={(e) => setValue(v => { return { ...v, age: Number(e.target.value) } })}
+                            onBlur={() => dispatcherValue({type: 'FOCUSOUT_AGE'})}
+                            onChange={(e) => dispatcherValue({type: 'UPDATE_AGE', val: Number(e.target.value)})}
                         />
                     </div>
-                    <div className='errorMsg'> {!isValid[1] && `Por favor, indique uma idade válido.`}</div>
+                    <div className='errorMsg'> {!Value.age.isValid && `Por favor, indique uma idade válido.`}</div>
                 </div>
                 <div className='formControlSubmit'>
                     <input type='submit' className='formControlSubmitButton' value='Salvar' />
